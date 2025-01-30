f9c520058b6ffc59a4ee4cd418f7ea48532295ad
Mitihl <vasanimithil999@gmail.com>
2025-01-29 20:02:19 -0500

    adds router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from './css/globalStyles'
 import theme from './css/theme'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import Placeholder from './pages/Placeholder'
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Placeholder />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<div>ROOT PAGE</div>} />
+          <Route path="/placeholder" element={<Placeholder />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
 
 export default App
+
+// Use the links below to navigate between pages:
+//   const navigate = useNavigate();
+// navigate("/profile")
